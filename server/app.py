c36be06b428557c51fba9e0eb0ea77c2ee6a2e2d
BakelohNoel <bakelohnoel@gmail.com>
2024-03-12 15:49:17 +0300

    app.py

--- server/app.py
+++ server/app.py
@@ -1,29 +1,84 @@
-#!/usr/bin/env python3
-
+from flask import Flask, request, jsonify, abort
 from models import db, Restaurant, RestaurantPizza, Pizza
-from flask_migrate import Migrate
-from flask import Flask, request, make_response
-from flask_restful import Api, Resource
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASE = os.environ.get(
-    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-
-migrate = Migrate(app, db)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///restaurants.db'
 db.init_app(app)
 
+def error_response(message, status_code):
+    response =  jsonify({"error": message})
+    response.status_code =  status_code
+    return response
+    
+    
+def validate_restaurant_pizza_data(data):
+    price = data.get('price')
+    pizza_id = data.get('pizza_id')
+    restaurant_id = data.get('restaurant_id')
+    
+    if not all([price, pizza_id, restaurant_id]):
+        return False, "Missing values in the request."
+    if not (1  <= int(price) <= 999):
+        return False, "'Price' must be a number between 1 and 999." 
+        
+    # try:
+    #     Pizza.query.filter_by(id=int(pizza_id)).one() # Checks that there is a pizza with this id
+    #     Restaurant.query.filter_by(id=int(restaurantage)).one() # Checks that there is a restaurant with this id
+    #     Restaurant.query.filter_by(id=int(restaurment_id)).one() # Checks that there is a restaurant with this id  
+    return True, ""    
 
-@app.route('/')
-def index():
-    return '<h1>Code challenge</h1>'
+@app.route('/restaurants', methods=['GET'])
+def get_all_restaurants():
+    restaurants = Restaurant.query.all()
+    return  jsonify([restaurant.to _dict() for restaurant in restaurants]), 200
 
+@app.route('/restaurants/<int:id>', methods=['GET'])
+def get_restaurant_by_id(id):
+    restaurant = Restaurant.query.get(id)
+    if restaurant:
+        return  jsonify(restaurant.to_dict(include_pizzas=True)), 200
+    else:
+        return error_response("Restaurant does not exist.", 404)    
 
+@app.route('/restaurants', methods=['DELETE'])
+def delete_restaurant(id):
+    restaurant = Restaurant.query.get(id)
+    if restaurant:
+        db.session.delete(restaurant)
+        db.session.commit()
+        return '', 204
+    else:
+        return error_response("The restaurant does not exist", 404)    
+
+@app.route('/pizzas' ,methods=['GET'])
+def get_pizzas():
+    pizzas =  Pizza.query.all()
+    return jsonify([pizza.to_dict() for pizza in pizzas]), 200
+
+@app.route('/restaurant_pizzas', methods=['POST'])
+def add_new_pizza_to_restaurant():
+    is_valid, error_message = validate_restaurant_pizza_data(data)
+    if not is_valid:
+        return error_response(error_message, 400)
+    
+    pizza = Pizza.query.get(data['pizza_id'])
+    restaurant = Restaurant.query.get(data['restaurant_id'])
+    
+    if  not pizza or not restaurant:
+       return error_response('Pizza or Restaurant does not exist', 400)
+   
+    try:
+        
+        new_pizza_in_restaurant = Restaurant_Pizza(
+           price=data['price'],
+           pizza_id=data['pizza_id'],
+           restaurant_id=data['restaurant_id']
+       )
+        db.session.add(new_pizza_in_restaurant)
+        db.session.commit()
+        return jsonify(new_pizza_in_restaurant.to_dict()), 201
+    except ValueError as e:
+        return error_response(str(e), 500)
+    
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5555, debug=True)          
